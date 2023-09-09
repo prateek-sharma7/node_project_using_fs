c263ff8ac31fb66b34189fa630417205091fc7b5
Prateek <sharmaprateek499@gmail.com>
2023-09-09 15:14:50 +0530

    creating and serving html services

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
+//Page not found
 app.use((req, res, next) => {
   res.status(404).send("<h1>Page not found</h1>");
 });
