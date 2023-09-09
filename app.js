3b3ed3fa29d2b3730c98fadcab70f50c7b9b1792
Prateek <sharmaprateek499@gmail.com>
2023-09-09 08:19:23 +0530

    understanding of middlewares

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer((req, res) => {
-  console.log(req);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
 
-server.listen(3000);
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express!</h1>");
+});
+
+app.listen(3000);
