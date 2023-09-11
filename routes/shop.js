dbbccee61fe5d1925e3f4738b7db335ebf25b8ee
Prateek <sharmaprateek499@gmail.com>
2023-09-11 13:46:47 +0530

    dynamic data using template engines

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const adminData = require("./admin");
 const rootDir = require("../util/path");
 
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
