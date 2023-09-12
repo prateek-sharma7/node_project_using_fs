4b5628a2eec48806e6ed744a1580bc828cb1139e
Prateek <sharmaprateek499@gmail.com>
2023-09-12 17:09:49 +0530

    dynamic routes and advanced models

--- models/product.js
+++ models/product.js
@@ -1,10 +1,13 @@
-const fs = require("fs");
-const path = require("path");
-const rootDir = require("../util/path");
+const fs = require('fs');
+const path = require('path');
 
-const p = path.join(rootDir, "data", "products.json");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -15,14 +18,18 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,21 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-const productscontroller = require("../controllers/products");
+router.get('/', shopController.getIndex);
 
-router.get("/", productscontroller.getProducts);
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
