99dcd4de50a9a6b2b330b75c8c248c53d67c0a40
Prateek <sharmaprateek499@gmail.com>
2023-09-12 16:38:05 +0530

    updated current state by adding templating engine

--- models/product.js
+++ models/product.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
 const path = require("path");
+const rootDir = require("../util/path");
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
-);
+const p = path.join(rootDir, "data", "products.json");
 
 const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
